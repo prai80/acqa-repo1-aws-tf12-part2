a513f25aa3613b26a448ea323748b50dd370e092
prai80 <106154428+prai80@users.noreply.github.com>
2022-12-15 15:04:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,11 @@ variable "acqaPrefix" {
     type = string
     default = "acqa-test"
 }
+tags = {
+    "jet:metadata:owner"        = "Infosec"
+    "jet:metadata:dept"         = "Cloudsec"
+    "jet:metadata:feature"      = "Cloudquery"
+}
 
 # Commenting validation as BAT is using TF .12 and it is TF13 feature
 # variable "var1" {
